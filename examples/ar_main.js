fb65d91c3588d4c711de4f633de45c76d4f895a6
Raghav Joshi <gamescocclans123@gmail.com>
2025-04-02 15:24:51 +0530

    some more errors

--- examples/ar_main.js
+++ examples/ar_main.js
@@ -2,13 +2,14 @@
 import * as THREE from 'three';
 import { ARButton } from 'three/addons/webxr/ARButton.js';
 import { XRControllerModelFactory } from 'three/addons/webxr/XRControllerModelFactory.js';
-import { initAR, render, animate } from './ar_core.js?v=3';
-import { setupEventListeners, createInteractivePlane } from './ar_interaction.js?v=3';
-import { loadVideoTexture } from './ar_media.js?v=3';
-import { createButton, createControlPanel, updateControlPanel } from './ar_ui_wrapper.js';
-import * as WebDOM from './ar_web_dom.js?v=3';
-import * as WebOverlay from './ar_web_overlay.js?v=3';
-import * as WebMessaging from './ar_web_messaging.js?v=3';
+import { initAR, render, animate } from './ar_core.js?v=4';
+import { setupEventListeners } from './ar_interaction.js?v=4';
+import { loadVideoTexture } from './ar_media.js?v=4';
+import { createButton, createControlPanel, updateControlPanel } from './ar_ui_wrapper.js?v=4';
+import * as WebDOM from './ar_web_dom.js?v=4';
+import * as WebOverlay from './ar_web_overlay.js?v=4';
+import * as WebMessaging from './ar_web_messaging.js?v=4';
+import { createInteractivePlane } from './ar_interaction_plane.js?v=4';
 
 // Wait for DOM content to be loaded before initializing
 document.addEventListener('DOMContentLoaded', () => {
